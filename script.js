033f6374f93895ce45286d5bf03bea8978c41efb
Václav Doležal <bc.va.do@gmail.com>
2022-03-26 07:11:29 +0100

    created the array usage example

---
+++ script.js
@@ -0,0 +1,124 @@
+// task 10
+
+/*
+function myFunction(num1,num2) {
+    for (let index = num1; index <= num2; index++) {
+        console.log(index);
+    }
+}
+
+
+
+
+
+function mainFunction() {
+    let num1 = prompt("Enter number one: ");
+    let num2 = prompt("Enter number two");
+    myFunction(num1,num2);
+}
+
+mainFunction()
+*/
+
+// task 11
+
+
+/*
+
+function myFunction(num1,num2) {
+    if (num1 > num2) {
+        for (let index = num1;index >= num2;index--) {
+           console.log(index);
+        } 
+    } else if (num1 < num2) {
+        for (index = num1; index <= num2; index++) {
+            console.log(index);
+        }
+    } else if (num1 == num2) {
+        console.log("Entered number are equal each other");
+    }
+}
+
+
+
+
+function mainFunction() {
+    let num1 = prompt("Enter number one: ");
+    let num2 = prompt("Enter number two: ");
+    myFunction(num1,num2);
+}
+
+
+
+mainFunction();
+*/
+
+// task 12
+
+
+
+
+/*
+function myFunction(array01,word01,)
+
+
+
+
+function mainFunction() {
+    let array01 = new array();
+    let stop = false;
+    while  {
+
+    }
+
+}
+
+
+
+
+mainFunction()
+*/
+
+
+/*
+let array01 = new Array();
+array01.push(prompt("Enter number: "));
+console.log(array01);
+*/
+
+
+/*
+let array01 = []
+array01.push(prompt("Enter item: "));
+console.log(array01);
+*/
+
+
+/*
+let stop = true;
+let array01 = []
+let item;
+while (stop == true) {
+    item = prompt("Enter item of list or 'STOP'.");
+    if (item == "STOP") {
+        break
+    }
+    array01.push(item);
+    //console.log(array01);
+} 
+
+
+console.log(array01)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
